c35fbf55756bc847a86edae97169ef400099a401
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-06-09 17:51:00 +0300

    Minor

--- Teamcity.CSharpInteractive/Info.cs
+++ Teamcity.CSharpInteractive/Info.cs
@@ -1,8 +1,10 @@
 // ReSharper disable ClassNeverInstantiated.Global
 namespace Teamcity.CSharpInteractive
 {
+    using System;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
 
     [ExcludeFromCodeCoverage]
     internal class Info : IInfo
@@ -45,7 +47,7 @@ namespace Teamcity.CSharpInteractive
                 new Text("    Ctrl-C       ", Color.Header), new Text("Exit the REPL."), Text.NewLine,
                 new Text("REPL commands:", Color.Header), Text.NewLine,
                 new Text("    #help        ", Color.Header), new Text("Display help on available commands and key bindings."), Text.NewLine,
-                new Text("    #l           ", Color.Header), new Text("Set logging verbosity level normal or trace, e.g. "), new Text("#l trace", Color.Details), new Text("."), Text.NewLine,
+                new Text("    #l           ", Color.Header), new Text($"Set verbosity level {string.Join(", ", Enum.GetValues(typeof(VerbosityLevel)).OfType<VerbosityLevel>().Select(i => i.ToString()))}, e.g. "), new Text("#l trace", Color.Details), new Text("."), Text.NewLine,
                 new Text("Script directives:", Color.Header), Text.NewLine,
                 new Text("    #r           ", Color.Header), new Text("Add a reference to a NuGet package or specified assembly and all its dependencies, e.g., "), new Text("#r MyPackage 1.2.3", Color.Details), new Text(" or "), new Text("#r \"myLib.dll\"", Color.Details), new Text("."), Text.NewLine,
                 new Text("    #load        ", Color.Header), new Text("Load specified script file and execute it, e.g. "), new Text("#load \"myScript.csx\"", Color.Details), new Text("."), Text.NewLine
