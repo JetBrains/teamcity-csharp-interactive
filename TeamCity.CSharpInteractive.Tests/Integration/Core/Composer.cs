008b7fc316c4a643c535a81220999925bcaa406b
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2021-10-19 17:47:35 +0300

    Update deps

--- TeamCity.CSharpInteractive.Tests/Integration/Core/Composer.cs
+++ TeamCity.CSharpInteractive.Tests/Integration/Core/Composer.cs
@@ -9,15 +9,15 @@ namespace TeamCity.CSharpInteractive.Tests.Integration.Core
 
     internal static partial class Composer
     {
-        [Conditional("DI")]
-        private static void Setup() => DI.Setup()
-            .Default(Lifetime.Singleton)
-            .Bind<string>(WorkingDirectory).To(_ => Environment.CurrentDirectory)
-            .Bind<char>(ArgumentsSeparatorChar).To(_ => ' ')
-            .Bind<IFileSystem>().To<FileSystem>()
-            .Bind<IProcessFactory>().To<ProcessFactory>()
-            .Bind<Func<System.Diagnostics.Process, IProcess>>().To(_ => new Func<System.Diagnostics.Process, IProcess>(process => new Process(process)))
-            .Bind<IProcessRunner>().To<CsiProcessRunner>()
-            .Bind<IServiceMessageParser>().To<ServiceMessageParser>();
+        private static void Setup() => 
+            DI.Setup()
+                .Default(Lifetime.Singleton)
+                .Bind<string>(WorkingDirectory).To(_ => Environment.CurrentDirectory)
+                .Bind<char>(ArgumentsSeparatorChar).To(_ => ' ')
+                .Bind<IFileSystem>().To<FileSystem>()
+                .Bind<IProcessFactory>().To<ProcessFactory>()
+                .Bind<Func<System.Diagnostics.Process, IProcess>>().To(_ => new Func<System.Diagnostics.Process, IProcess>(process => new Process(process)))
+                .Bind<IProcessRunner>().To<CsiProcessRunner>()
+                .Bind<IServiceMessageParser>().To<ServiceMessageParser>();
     }
 }
