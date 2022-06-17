2302d75e1add0bef16908fb21832927eb975b1ed
NikolayPianikov <Nikolay.Pianikov@jetbrains.com>
2022-06-17 17:45:30 +0300

    Use default nuget settimgs for NuGet oparations by default

--- TeamCity.CSharpInteractive/NuGetEnvironment.cs
+++ TeamCity.CSharpInteractive/NuGetEnvironment.cs
@@ -3,75 +3,44 @@ namespace TeamCity.CSharpInteractive;
 
 using System.Diagnostics.CodeAnalysis;
 
-internal class NuGetEnvironment : INuGetEnvironment, ITraceSource, IDisposable
+[ExcludeFromCodeCoverage]
+internal class NuGetEnvironment : INuGetEnvironment, ITraceSource
 {
     private readonly IEnvironment _environment;
-    private readonly IHostEnvironment _hostEnvironment;
-    private readonly IUniqueNameGenerator _uniqueNameGenerator;
-    private readonly ICleaner _cleaner;
     private readonly ISettings _settings;
-    private string? _packagePath;
-    private IDisposable _packagePathToken = Disposable.Empty;
 
     public NuGetEnvironment(
         IEnvironment environment,
-        IHostEnvironment hostEnvironment,
-        IUniqueNameGenerator uniqueNameGenerator,
-        ICleaner cleaner,
         ISettings settings)
     {
         _environment = environment;
-        _hostEnvironment = hostEnvironment;
-        _uniqueNameGenerator = uniqueNameGenerator;
-        _cleaner = cleaner;
         _settings = settings;
     }
 
-    public IEnumerable<string> Sources => _settings.NuGetSources.Concat(new[] {@"https://api.nuget.org/v3/index.json"});
-
-    public IEnumerable<string> FallbackFolders =>
-        FallbackFoldersFromEnv
+    public IEnumerable<string> Sources => 
+        _settings.NuGetSources
+            .Concat(NuGet.Configuration.SettingsUtility.GetEnabledSources(GetNuGetSettings()).Select(i => i.Source))
             .Distinct();
 
-    public string PackagesPath
-    {
-        get
-        {
-            var path = _hostEnvironment.GetEnvironmentVariable("NUGET_PACKAGES")?.Trim();
-            if (!string.IsNullOrEmpty(path))
-            {
-                return path;
-            }
+    public IEnumerable<string> FallbackFolders =>
+        NuGet.Configuration.SettingsUtility.GetFallbackPackageFolders(GetNuGetSettings()).Distinct();
 
-            if (_packagePath != null)
-            {
-                return _packagePath;
-            }
-
-            _packagePath = Path.Combine(_environment.GetPath(SpecialFolder.Temp), _uniqueNameGenerator.Generate());
-            _packagePathToken = _cleaner.Track(_packagePath);
-            return _packagePath;
-        }
-    }
-
-    public void Dispose() => _packagePathToken.Dispose();
-
-    [ExcludeFromCodeCoverage]
+    public string PackagesPath =>
+        NuGet.Configuration.SettingsUtility.GetGlobalPackagesFolder(GetNuGetSettings());
+    
     public IEnumerable<Text> Trace
     {
         get
         {
             yield return new Text($"PackagesPath: {PackagesPath}");
             yield return Text.NewLine;
-            yield return new Text($"NuGetSources: {string.Join(";", Sources)}");
+            yield return new Text($"NuGetSources: {string.Join(';', Sources)}");
             yield return Text.NewLine;
-            yield return new Text($"NuGetFallbackFolders: {string.Join(";", FallbackFolders)}");
+            yield return new Text($"NuGetFallbackFolders: {string.Join(';', FallbackFolders)}");
             yield return Text.NewLine;
         }
     }
 
-    private IEnumerable<string> FallbackFoldersFromEnv => _hostEnvironment
-        .GetEnvironmentVariable("NUGET_FALLBACK_PACKAGES")
-        ?.Split(';', StringSplitOptions.RemoveEmptyEntries)
-        .Select(i => i.Trim()) ?? Enumerable.Empty<string>();
+    private NuGet.Configuration.ISettings GetNuGetSettings() =>
+        NuGet.Configuration.Settings.LoadDefaultSettings(_environment.GetPath(SpecialFolder.Script));
 }
